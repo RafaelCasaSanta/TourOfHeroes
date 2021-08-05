67ec9124f2a66667344e3d511580e806001050b3
Rafael Casa <rafael.casasanta@edu.unifil.br>
2021-08-05 10:45:59 -0300

    Display List

--- TOH/src/app/heroes/heroes.component.ts
+++ TOH/src/app/heroes/heroes.component.ts
@@ -7,12 +7,22 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  heroes = HEROES
 
-  constructor() { }
+export class HeroesComponent implements OnInit {
+
+    heroes = HEROES
+
+    constructor() { }
 
   ngOnInit() {
   }
 
+  selectedHero?: Hero;
+    onSelect(hero : Hero): void {
+      this.selectedHero = hero;
+    }
+
+
+
 }
+
